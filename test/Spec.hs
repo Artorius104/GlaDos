ee8e615d35b64c4c9f4fd6a86a88d8a4b5822c04
Divin <72012711+Artorius104@users.noreply.github.com>
2023-03-05 22:19:25 +0100

    Update Spec.hs

--- test/Spec.hs
+++ test/Spec.hs
@@ -9,7 +9,3 @@ main = hspec $ do
         describe "Parser" $ do
             it "ParseAst" $ do
                 stringToAst "foo = 21 foo * 2" `shouldBe` [BinaryOp "=" (Symbol "foo") (Int 21),BinaryOp "*" (Symbol "foo") (Int 2)]
-            it "ParseAst" $ do
-                stringToAst "foo * 42" `shouldBe` [BinaryOp "*" (Symbol "foo") (Int 42)]
-            it "ParseAst" $ do
-                stringToAst "10 / 2" `shouldBe` [BinaryOp "/" (Int 10) (Int 2)]
